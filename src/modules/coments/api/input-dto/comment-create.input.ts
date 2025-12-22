460923a2413564a36c94b9030af8f40b432a7317
shapl_xlbobtq <sergeshapluk@gmail.com>
2025-12-22 17:40:20 +0300

    commit

--- src/modules/coments/api/input-dto/comment-create.input.ts
+++ src/modules/coments/api/input-dto/comment-create.input.ts
@@ -1,9 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 import { Trim } from '../../../../core/decorators/transform/trim';
 
 export class CommentCreateInputDto {
-  @IsString()
-  // @Length(passwordConstraints.minLength, passwordConstraints.maxLength)
+  @IsString({ message: 'content must be a string' })
+  @Length(20, 300, { message: 'content must be between 20 and 300 characters' })
   @Trim()
   content: string;
 }
--- src/modules/coments/api/input-dto/comment-update.input.ts
+++ src/modules/coments/api/input-dto/comment-update.input.ts
@@ -1,14 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 import { Trim } from '../../../../core/decorators/transform/trim';
 import { CommentCreateInputDto } from './comment-create.input';
 
 export class CommentUpdateInputDto implements CommentCreateInputDto {
-  // @IsString()
-  // id: string;
-
-  @IsString()
-  // @Length(passwordConstraints.minLength, passwordConstraints.maxLength)
+  @IsString({ message: 'content must be a string' })
+  @Length(20, 300, { message: 'content must be between 20 and 300 characters' })
   @Trim()
   content: string;
-
 }
