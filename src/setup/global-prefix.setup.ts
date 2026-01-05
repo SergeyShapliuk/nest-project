65a61fb9ac4087e6b73519cd95a96627e9a185b6
shapl_xlbobtq <sergeshapluk@gmail.com>
2026-01-05 20:03:36 +0300

    add postgres

--- src/setup/global-prefix.setup.ts
+++ src/setup/global-prefix.setup.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 
-// export const GLOBAL_PREFIX = 'api';
-export const GLOBAL_PREFIX_SA = 'sa';
+export const GLOBAL_PREFIX = 'api';
+// export const GLOBAL_PREFIX_SA = 'sa';
 
 export function globalPrefixSetup(app: INestApplication) {
   //специальный метод, который добавляет ко всем маршрутам /GLOBAL_PREFIX
