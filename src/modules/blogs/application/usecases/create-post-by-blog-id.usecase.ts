80932fe567cfe0634f1d9059291191740f0810e7
shapl_xlbobtq <sergeshapluk@gmail.com>
2026-01-08 21:56:22 +0300

    add 18 home task

--- src/modules/blogs/application/usecases/create-post-by-blog-id.usecase.ts
+++ src/modules/blogs/application/usecases/create-post-by-blog-id.usecase.ts
@@ -3,6 +3,8 @@ import { CreatePostDto } from '../../../posts/dto/create-post.dto';
 import { PostsQwRepository } from '../../../posts/infrastructure/query/posts.query.repository';
 import { PostViewDto } from '../../../posts/api/view-dto/posts.view-dto';
 import { PostsRepository } from '../../../posts/infrastructure/posts.repository';
+import { BlogsRepository } from '../../infrastructure/blogs.repository';
+import { NotFoundException } from '@nestjs/common';
 
 export class CreatePostByBlogIdCommand {
   constructor(public dto: CreatePostDto) {
@@ -15,11 +17,16 @@ export class CreatePostByBlogIdUseCase
   constructor(
     private postsQwRepository: PostsQwRepository,
     private postsRepository: PostsRepository,
+    private blogsRepository: BlogsRepository,
   ) {
   }
 
   async execute({ dto }: CreatePostByBlogIdCommand): Promise<PostViewDto> {
     console.log('❤️ Execute');
+    const blog = await this.blogsRepository.findOrNotFoundFail(dto.blogId);
+    if (!blog) {
+      throw new NotFoundException(`Blog with id ${dto.blogId} not found`);
+    }
     const createPostByBlogId = await this.postsRepository.create(dto);
 
 
